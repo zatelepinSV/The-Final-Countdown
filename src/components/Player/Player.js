7f146777e43d2d987b50fa08e67bdcf4f9bd6c9b
sergey <sergey.zatsialepin@gmail.com>
2023-12-03 23:04:45 +0300

    add Player component

---
+++ src/components/Player/Player.js
@@ -0,0 +1,14 @@
+const Player = () => {
+
+  return (
+    <section>
+      <h2>Welcome unknown entity</h2>
+      <p>
+        <input type="text" />
+        <button>Set Name</button>
+      </p>
+    </section>
+  );
+};
+
+export {Player};
