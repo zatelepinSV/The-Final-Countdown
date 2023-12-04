716a2ac4129fa2cdb9bc0f2c22550fa8d913d011
sergey <sergey.zatsialepin@gmail.com>
2023-12-04 21:44:58 +0300

    update challenge

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ const App = () => {
       <Player/>
       <div className={classes.challenges}>
         <TimerChallenge title="Easy" targetTime={1} />
-        <TimerChallenge title="Easy" targetTime={1} />
+        <TimerChallenge title="Normal" targetTime={5} />
+        <TimerChallenge title="Hard" targetTime={10} />
+        <TimerChallenge title="Pros only" targetTime={15} />
       </div>
     </>
   );
--- src/components/TimerChallenge/TimerChallenge.js
+++ src/components/TimerChallenge/TimerChallenge.js
@@ -1,20 +1,36 @@
 import classes from "./TimerChallenge.module.css";
+import { useState, useRef } from "react";
 
 export const TimerChallenge = ({ title, targetTime }) => {
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+  const timer = useRef();
+
+  const startTimerStartHandler = () => {
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+    setTimerStarted(true);
+  }
+
+  const stopTimerHandler = () => {
+    clearTimeout(timer.current);
+  }
 
   return (
     <section className={classes.challenge}>
-      <h2 className={classes.difficulty}>{title}</h2>
+      <h2 className={classes.title}>{title}</h2>
+      {timerExpired && <p>You Lost</p>}
       <p className={classes.challengeTime}>
         {targetTime} second{targetTime > 1 ? 's' : ''}
       </p>
       <p>
-        <button className={classes.challengeButton}>
-          Start Challenge
+        <button className={classes.challengeButton} onClick={timerStarted ? stopTimerHandler : startTimerStartHandler}>
+          {timerStarted ? 'Stop' : 'Start'} Start Challenge
         </button>
       </p>
-      <p className="">
-        Time is running / Timer in active
+      <p className={timerStarted ? classes.active : undefined}>
+        {timerStarted ? 'Time is running...' : 'Timer in active'}
       </p>
     </section>
   );
